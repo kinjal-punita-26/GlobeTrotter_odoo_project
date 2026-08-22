6d826eb22d80bacdb1990e957f8ae3b7dd32dc9a
kinjal-punita-26 <kinjalvasava993@gmail.com>
2026-08-22 10:55:00 +0530

    Initial project setup

---
+++ Globetrotter/Tplanner/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TplannerConfig(AppConfig):
+    name = 'Tplanner'
---
+++ Globetrotter/Tplanner/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def Tplanner(request):
+    return HttpResponse("Hello, Django!")
+
+# Create your views here.
